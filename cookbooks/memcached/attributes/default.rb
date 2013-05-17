e5e638f6cf0d037bb2778ecc01551c4f3d2a3bfe
Christoph Bünte <info@christophbuente.de>
2013-05-17 13:07:25 +0200

    Do not expose memcache to the outside world.

--- cookbooks/memcached/attributes/default.rb
+++ cookbooks/memcached/attributes/default.rb
@@ -20,5 +20,5 @@
 default['memcached']['memory'] = 256
 default['memcached']['port'] = 11211
 default['memcached']['user'] = "nobody"
-default['memcached']['listen'] = "0.0.0.0"
+default['memcached']['listen'] = "127.0.0.1"
 default['memcached']['maxconn'] = 1024
