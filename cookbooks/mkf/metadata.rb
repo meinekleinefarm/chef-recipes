2e64a4b23d6e7fa93e312a14233813d8d6f80663
Christoph Bünte <info@christophbuente.de>
2013-07-26 15:26:52 +0200

    Remove direct dependency.

--- cookbooks/mkf/metadata.rb
+++ cookbooks/mkf/metadata.rb
@@ -5,7 +5,6 @@ description      "Deploys our mkf rails application to the server."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 depends          "rbenv"
-depends          "users"
 depends          "database"
 depends          "application"
 depends          "application_ruby"
