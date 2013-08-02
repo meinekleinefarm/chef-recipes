2a5060a430a6f5aae8d8e74ecbd0f3427ee73b65
Christoph Bünte <info@christophbuente.de>
2013-08-02 16:25:53 +0200

    Raise memory and turn off vacuum.

--- cookbooks/postgresql/attributes/default.rb
+++ cookbooks/postgresql/attributes/default.rb
@@ -148,12 +148,13 @@ when 'debian'
   default['postgresql']['config']['port'] = 5432
   default['postgresql']['config']['max_connections'] = 100
   default['postgresql']['config']['unix_socket_directory'] = '/var/run/postgresql'
-  default['postgresql']['config']['shared_buffers'] = '24MB'
+  default['postgresql']['config']['shared_buffers'] = '512MB'
   default['postgresql']['config']['max_fsm_pages'] = 153600 if node['postgresql']['version'].to_f < 8.4
   default['postgresql']['config']['log_line_prefix'] = '%t '
   default['postgresql']['config']['datestyle'] = 'iso, mdy'
   default['postgresql']['config']['default_text_search_config'] = 'pg_catalog.english'
   default['postgresql']['config']['ssl'] = true
+  default['postgresql']['config']['autovacuum'] = false
 when 'rhel', 'fedora', 'suse'
   default['postgresql']['config']['listen_addresses'] = 'localhost'
   default['postgresql']['config']['max_connections'] = 100
