391e62c582439c50ef58e42fa79d86afce175a12
Christoph Bünte <info@christophbuente.de>
2013-11-02 22:52:16 +0100

    Import application_ruby version 2.1.4

--- cookbooks/application_ruby/metadata.rb
+++ cookbooks/application_ruby/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Deploys and configures Ruby-based applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.1.0"
+version          "2.1.4"
 
 %w{ unicorn apache2 passenger_apache2 }.each do |cb|
   depends cb
@@ -12,3 +12,4 @@ end
 
 depends "application", "~> 3.0"
 depends "runit", "~> 1.0"
+depends "logrotate", "~> 1.0"
